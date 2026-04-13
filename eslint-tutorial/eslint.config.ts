3ed1409160333a99d29510f29b9d3bb3cd49a601
nuco-s-kawaguchi <shusei_kawaguchi@nuco.co.jp>
2026-04-13 14:54:40 +0900

    eslint-tutorial

---
+++ eslint-tutorial/eslint.config.ts
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default defineConfig([
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { projectService: true },
+    },
+  },
+  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    rules: {
+      "no-console": "warn",
+      camelcase: ["warn", { properties: "never" }],
+      "@stylistic/semi": ["warn", "always"],
+      "@typescript-eslint/switch-exhaustiveness-check": "warn",
+    },
+  },
+]);
+
---
+++ eslint-tutorial/src/hello-world.ts
@@ -0,0 +1,10 @@
+type Choice = "A" | "B" | "C";
+
+export function func(choice: Choice) {
+  switch (choice) {
+    case "A":
+      break;
+    case "B":
+      break;
+  }
+}
